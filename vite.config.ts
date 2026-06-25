4d38f88ff204885fef3d96565a7c261a36f5e9bd
osama1633 <osamafares8070@gmail.com>
2026-06-26 01:05:30 +0300

    Fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,22 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
+  nitro: process.env.VERCEL ? true : false,
+
   tanstackStart: {
-    // هنغير الإعداد لـ static بدل server
-    ssr: false,
+    server: {
+      entry: "server",
+    },
+  },
+
+  vite: {
+    plugins: [
+      nitro({
+        preset: "vercel",
+      }),
+    ],
   },
 });
